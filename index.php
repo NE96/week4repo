cb40730f96cc018771f823bc0107de8242700449
Norman Eidelman <ne58@afsconnect1.njit.edu>
2017-10-01 17:17:18 -0400

    First

--- index.php
+++ index.php
@@ -9,4 +9,7 @@ $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
 print_r($year)
 
+$date = str_replace("-", "/", $date);
+echo "The value of \$date: ".$date."<br>";
+
 ?>
